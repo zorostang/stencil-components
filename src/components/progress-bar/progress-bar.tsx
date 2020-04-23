a5e13bb5d750dccc3a808acabf22198158e74579
Gino Bernardi <ginobernardi@att.net>
2020-04-23 13:53:48 -0500

    tested polling request inside progress bar, changed color to match select

--- src/components/progress-bar/progress-bar.tsx
+++ src/components/progress-bar/progress-bar.tsx
@@ -1,10 +1,11 @@
-import { Component, Element, Prop, h } from '@stencil/core';
+import { Component, Element, Prop, h } from "@stencil/core";
 import Chart from "chart.js";
+import Request from "axios-request-handler";
 
 @Component({
-  tag: 'nwc-progress-bar',
-  styleUrl: 'progress-bar.css',
-  shadow: false
+  tag: "nwc-progress-bar",
+  styleUrl: "progress-bar.css",
+  shadow: false,
 })
 export class DrawingPad {
   @Prop() value;
@@ -13,43 +14,49 @@ export class DrawingPad {
   canvas: HTMLCanvasElement;
   maxvalue = 100;
   componentDidLoad() {
+    this.getYesResults();
     const value = this.value;
     const maxvalue = this.maxvalue;
     const canvas = this.el.querySelector("canvas");
     canvas.height = 20;
     var test = new Chart(canvas, {
-      type: 'horizontalBar',
+      type: "horizontalBar",
       data: {
         labels: [],
-        datasets: [{
-          data: [value],
-          backgroundColor: "rgba(51,230,125,1)"
-        }, {
-          data: [maxvalue - value],
-          backgroundColor: "lightgrey",
-        }, ]
+        datasets: [
+          {
+            data: [value],
+            backgroundColor: "#7500c0",
+          },
+          {
+            data: [maxvalue - value],
+            backgroundColor: "lightgrey",
+          },
+        ],
       },
       options: {
         legend: {
-          display: false
+          display: false,
         },
         tooltips: {
-          enabled: false
+          enabled: false,
         },
         scales: {
-          xAxes: [{
-            display: false,
-            stacked: true
-          }],
-          yAxes: [{
-            display: false,
-            stacked: true
-          }],
-        } // scales
-      } // options
+          xAxes: [
+            {
+              display: false,
+              stacked: true,
+            },
+          ],
+          yAxes: [
+            {
+              display: false,
+              stacked: true,
+            },
+          ],
+        }, // scales
+      }, // options
     });
-    console.log(canvas)
-    console.log(test)
   }
 
   // render() {
@@ -63,8 +70,17 @@ export class DrawingPad {
   //     </div>
   //   );
   // }
+  getYesResults() {
+    const reviews = new Request("http://localhost:3000/questions/2");
+
+    reviews.poll(200).get((response) => {
+      console.log(response.data);
+      // you can cancel polling by returning false
+    });
+  }
   render() {
     return (
+      // todo: figure out why container was needed. component didnt render wo it
       <div class="canvas-container">
         <canvas></canvas>
       </div>
