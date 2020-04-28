3f4e5ec16858198a8e008dd1536f5b8b859843c1
gaudynights <mbalagia@gmail.com>
2020-04-28 11:15:56 -0500

    changed user form to take question id as prop instead of question text created fetch to be able to create user responses (hard coded to q1 yes) changed db schema to have a responses

--- src/components/nwc-survey-part/nwc-survey-part.tsx
+++ src/components/nwc-survey-part/nwc-survey-part.tsx
@@ -1,4 +1,4 @@
-import { Component, ComponentInterface, Host, h, Prop } from '@stencil/core';
+import { Component, ComponentInterface, Host, h, Prop, Element } from '@stencil/core';
 
 @Component({
   tag: 'nwc-survey-part',
@@ -11,40 +11,80 @@ export class NwcSurveyPart implements ComponentInterface {
 
   @Prop() question: string;
 
+  @Prop() questionId: number;
+
+  @Element() el: HTMLElement;
+
+  componentWillLoad(){
+    if(!this.admin){
+      fetch('//localhost:3000/questions/'+this.questionId)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          this.question=data.title
+        });
+    }
+  }
+  submitForm(questionText){
+    let data = {
+      title: questionText,
+      yes: 0,
+      no:0 
+    };
+    fetch('//localhost:3000/questions/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+  }
+  submitUserForm(questionId, userAnswer){
+    
+    let data = {
+        "questionId": questionId,
+        "response": userAnswer
+    };
+    fetch('//localhost:3000/responses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data.responses);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+
+  }
+
   render() {
     if(this.admin){
-      function submitForm(questionText){
-        let data = {
-          title: questionText,
-          yes: 0,
-          no:0 
-        };
-        fetch('//localhost:3000/questions/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log('Success:', data);
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
 
-      }
       return (
         <Host>
           <form id="adminform" onSubmit={
             (event)=>{ 
               let questionText = event.target[0].value;
               console.log(event.target[0].value);
-              console.log(event);
+              console.log('event.target:');
               console.log(event.target);
               event.preventDefault();
-              submitForm(questionText);
+              this.submitForm(questionText);
               }}>
             <label >
               Question 1:
@@ -60,7 +100,12 @@ export class NwcSurveyPart implements ComponentInterface {
     if(!this.admin){
       return(
         <Host>
-          <form>
+          <form id="user-form" onSubmit={(event)=>{
+            // console.log(this.el.shadowRoot.ge)
+            let userAnswer="yes";
+            event.preventDefault();
+            this.submitUserForm(1, userAnswer);
+          }}>
             <p>{this.question}</p>
             <input type="radio" id="yes" value="yes" name="user-answer" required></input>
             <label htmlfor="yes">Yes</label>
